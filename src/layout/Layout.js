c6af5234132072786526fde062dfe5f92776c22e
song <songnumber1@naver.com>
2024-02-26 23:12:02 +0900

    spinner 추가

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
 import '../assert/css/layout/layout.css';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Layout = ({ children }) => {
   return (
-    <div className="context">
-      <div className="main">
-        <Header />
-        {children}
+    <div>
+      <div>
+        <Spinner animation="grow" />
       </div>
-      <Footer />
+      <div className="context">
+        <div className="main">
+          <Header />
+          {children}
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
